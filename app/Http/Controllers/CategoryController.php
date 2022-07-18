8a19925c2b8a8c6718b8729b0ede687f112af0f3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-07-18 18:45:18 +0700

    Update Controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('categories.categories', [
             'title' => 'Semua Kategori'
         ]);
     }
-    public function category(){
-
+    public function category($slug)
+    {
+        $categories = Category::where('slug', '=', $slug);
+        return view('categories.category', [
+            'title' => 'Kategori',
+            'categories'=>$categories
+        ]);
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,8 +13,11 @@ class PostController extends Controller
             'title' => 'Postingan'
         ]);
     }
-    public function post()
+    public function post($slug)
     {
-        
+        $posts = Post::where('slug','=',$slug);
+        return view('posts.post',[
+            'title'=>'Postingan'
+        ]);
     }
 }
